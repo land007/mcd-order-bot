707f0f624796236d3bd5119cc52ba6570b48c7b9
阿七 <yiqiujia@hotmail.com>
2021-10-06 12:19:59 +0800

    Talk to the robot

---
+++ dialogue.py
@@ -0,0 +1,22 @@
+import json
+import secrets
+import requests
+
+
+def post(url, data=None):
+    data = json.dumps(data, ensure_ascii=False)
+    data = data.encode(encoding="utf-8")
+    r = requests.post(url=url, data=data)
+    r = json.loads(r.text)
+    return r
+
+
+sender = secrets.token_urlsafe(16)
+url = "http://localhost:5006/webhooks/rest/webhook"
+while True:
+    message = input("Your input ->  ")
+    data = {
+        "sender": sender,
+        "message": message
+    }
+    print(post(url, data))
